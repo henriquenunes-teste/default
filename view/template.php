4569d695e77af09667b290bafe1e140ef8f4726e
daniloalbernaz-web <danilo.albernaz@estudante.ifms.edu.br>
2026-04-30 08:23:18 -0400

    Update template.php

--- view/template.php
+++ view/template.php
@@ -7,10 +7,13 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
 <body>
-    <?php
-        include "menu.php";
-        include $view.".php";
-    ?>
+    <div class="container-fluid bg-danger align-items-center">
+        <?php
+            include "menu.php";
+            include $view.".php";
+        ?>
+    </div>
+    
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
