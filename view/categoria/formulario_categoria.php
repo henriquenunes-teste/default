5c9bb83377f783a62ecca16089b3611f5c869172
daniloalbernaz-web <danilo.albernaz@estudante.ifms.edu.br>
2026-05-14 07:26:41 -0400

    Update formulario_categoria.php

--- view/categoria/formulario_categoria.php
+++ view/categoria/formulario_categoria.php
@@ -1,17 +1,18 @@
-
+<div class="container-fluid">
+    <h1>Categorias</h1>
     <form action="<?php echo APP.'/categoria/gravar'; ?>" method="POST">
         <div class="mb-3">
             <label for="id" class="form-label">ID</label>
             <input readonly type="text" class="form-control" id="id" name="id" value="<?php echo $dados['id']; ?>">
         </div>
-
         <div class="mb-3">
             <label for="nome" class="form-label">Nome</label>
             <input type="text" class="form-control" id="nome" name="nome" value="<?php echo $dados['nome']; ?>">
         </div>
         <div class="mb-3">
             <label for="cor" class="form-label">Cor</label>
-            <input type="color" class="form-control" id="cor" name="cor" value="<?php echo $dados['cor']; ?>">
+            <input type="cor" class="form-control" id="cor" name="cor" value="<?php echo $dados['cor']; ?>">
         </div>
         <button type="submit" class="btn btn-primary">Gravar</button>
     </form>
+</div>
