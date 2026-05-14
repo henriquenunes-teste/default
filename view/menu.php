d4977bf82b1477ac5694742795f82872c1db66a6
daniloalbernaz-web <danilo.albernaz@estudante.ifms.edu.br>
2026-05-14 07:18:20 -0400

    Update menu.php

--- view/menu.php
+++ view/menu.php
@@ -1,19 +1,21 @@
-<div class="container-fluid bg-success text-center">
-  <div class="grid row row-cols-auto">
-    <div class="col-md-auto  ">
-      <a href="<?php echo APP."/pagamento/listar"?>" class="btn btn-primary mb-3">Pagamentos</a>
-    </div>
-    <div class="col-md-auto">
-      <a href="<?php echo APP."/categoria/listar"?>" class="btn btn-primary mb-3">Categoria</a>
-    </div>
-    <div class="col-md-auto">
-      <a href="<?php echo APP."/usuario/listar"?>" class="btn btn-primary mb-3">Usuarios</a>
-    </div>
-    <div class="col-md-auto">
-      <a href="<?php echo APP."/produto/listar"?>" class="btn btn-primary mb-3">Produtos</a>
-    </div>
-    <div class="col-md-auto">
-      <a href="<?php echo APP."/carrinho/listar"?>" class="btn btn-primary mb-3">Carrinho</a>
+<nav>
+  <div class="container-fluid text-center pt-3">
+    <div class="grid row row-cols-auto">
+      <div class="col-md-auto  ">
+        <a href="<?php echo APP."/pagamento/listar"?>" class="btn btn-primary mb-3">Pagamentos</a>
+      </div>
+      <div class="col-md-auto">
+        <a href="<?php echo APP."/categoria/listar"?>" class="btn btn-primary mb-3">Categoria</a>
+      </div>
+      <div class="col-md-auto">
+        <a href="<?php echo APP."/usuario/listar"?>" class="btn btn-primary mb-3">Usuarios</a>
+      </div>
+      <div class="col-md-auto">
+        <a href="<?php echo APP."/produto/listar"?>" class="btn btn-primary mb-3">Produtos</a>
+      </div>
+      <div class="col-md-auto">
+        <a href="<?php echo APP."/carrinho/listar"?>" class="btn btn-primary mb-3">Carrinho</a>
+      </div>
     </div>
   </div>
-</div>
+</nav>
