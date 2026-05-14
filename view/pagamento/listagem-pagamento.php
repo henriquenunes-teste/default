4eac9b06b1de4c89b2fef1bcd437c65b92d73513
daniloalbernaz-web <danilo.albernaz@estudante.ifms.edu.br>
2026-05-14 07:24:46 -0400

    Update listagem-pagamento.php

--- view/pagamento/listagem-pagamento.php
+++ view/pagamento/listagem-pagamento.php
@@ -1,32 +1,35 @@
-    <h1>Pagamentos</h1>
+<div class="container-fluid">
+    <h1 class="pb-3">Pagamentos</h1>
     <a href="<?php echo APP.'/pagamento/novo'?>" class="btn btn-success">Novo</a>
     <a href="<?php echo APP."/"; ?>" class="btn btn-success">Voltar</a>
-    <table class="table">
-        <thead>
-            <tr>
-                <th scope="col">ID</th>
-                <th scope="col">Valor</th>
-                <th scope="col">Taxa</th>
-                <th scope="col">Data</th>
-                <th scope="col">Editar</th>
-                <th scope="col">Deletar</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php 
-                foreach($pagamentos as $pagamento){
-                    echo "
-                        <tr>
-                            <td>".$pagamento["id"]."</td>
-                            <td>".$pagamento["valor"]."</td>
-                            <td>".$pagamento["taxa"]."</td>
-                            <td>".$pagamento["data"]."</td>
-                            <td><a class='btn btn-primary' href=".APP.'/pagamento/alterar/'.$pagamento["id"].">Editar</a></td>
-                            <td><a class='btn btn-danger' href=".APP.'/pagamento/deletar/'.$pagamento["id"].">X</a></td>
-                        </tr>
-                    ";
-                }
-            
-            ?>
-        </tbody>
-    </table>
+    <div class="pt-3">
+        <table class="table table-bordered table table-sm table align-middle">
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Valor</th>
+                    <th scope="col">Taxa</th>
+                    <th scope="col">Data</th>
+                    <th scope="col">Editar</th>
+                    <th scope="col">Deletar</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php 
+                    foreach($pagamentos as $pagamento){
+                        echo "
+                            <tr>
+                                <td>".$pagamento["id"]."</td>
+                                <td>".$pagamento["valor"]."</td>
+                                <td>".$pagamento["taxa"]."</td>
+                                <td>".$pagamento["data"]."</td>
+                                <td><a class='btn btn-primary' href=".APP.'/pagamento/alterar/'.$pagamento["id"].">Editar</a></td>
+                                <td><a class='btn btn-danger' href=".APP.'/pagamento/deletar/'.$pagamento["id"].">X</a></td>
+                            </tr>
+                        ";
+                    }               
+                ?>
+            </tbody>
+        </table>
+    </div>
+</div>
