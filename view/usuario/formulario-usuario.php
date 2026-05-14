bde10f21e45393b3309b5429fb0939151fb099f2
daniloalbernaz-web <danilo.albernaz@estudante.ifms.edu.br>
2026-05-14 07:20:34 -0400

    Update formulario-usuario.php

--- view/usuario/formulario-usuario.php
+++ view/usuario/formulario-usuario.php
@@ -1,21 +1,18 @@
-
-    <h1>Usuário</h1>
-
+<div class="container-fluid">
+    <h1>Usuários</h1>
     <form action="<?php echo APP."/usuario/gravar"?>" method="post">
-        <div>
+        <div class="mb-3">
             <label for="id" class="form-label">ID</label>
             <input readonly type="number" value="<?php echo $data["id"]; ?>" name="id" class="form-control">
         </div>
-
-        <div>
+        <div class="mb-3">
             <label for="nome" class="form-label">Nome</label>
             <input type="text" value="<?php echo $data["nome"]; ?>" name="nome" class="form-control">
         </div>
-
-        <div>
+        <div class="mb-3">
             <label for="senha" class="form-label">Senha</label>
             <input type="text" value="<?php echo $data["senha"]; ?>" name="senha" class="form-control">
-        </div>
-        
+        </div>        
         <button type="submit" class="btn btn-primary">Salvar</button>
     </form>
+</div>
