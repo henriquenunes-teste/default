55a716a34a53537bf10a1fd66ff5020b1afdc185
daniloalbernaz-web <danilo.albernaz@estudante.ifms.edu.br>
2026-05-14 07:26:16 -0400

    Update listagem_categoria.php

--- view/categoria/listagem_categoria.php
+++ view/categoria/listagem_categoria.php
@@ -1,34 +1,34 @@
-
-    <h1>Listagem de Categorias</h1>
+<div class="container-fluid">
+    <h1 class="pb-3">Categorias</h1>
     <a class="btn btn-primary" href="novo">Novo</a>    
-    <a href="<?php echo APP."/"; ?>" class="btn btn-success">Voltar</a>
-    <table class="table">
-        <thead>
-            <tr>
-                <th scope="col">ID</th>
-                <th scope="col">Nome</th>
-                <th scope="col">Cor</th>
-                <th scope="col">Excluir</th>
-                <th scope="col">Editar</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-                foreach($categorias as $categoria) {
-                    echo "
-                        <tr>
-                            <th>{$categoria['id']}</th>
-                            <th>{$categoria['nome']}</th>
-                            <td style='background-color: {$categoria['cor']};color: white;'>{$categoria['cor']}</td>
-                            <td><a class='btn btn-danger' href='excluir/{$categoria['id']}'>x</a></td>
-                            <td><a class='btn btn-primary' href='editar/{$categoria['id']}'>+</a></td>
-                            
-                        </tr>
-                    ";
-                }   
-                
-            ?>
-        </tbody>
-        
-    </table>
-   
+    <a href="<?php echo APP."/"; ?>" class="btn btn-success">Voltar</a>   
+    <div class="pt-3"> 
+        <table class="table table-bordered table table-sm table align-middle">
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Cor</th>
+                    <th scope="col">Excluir</th>
+                    <th scope="col">Editar</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                    foreach($categorias as $categoria) {
+                        echo "
+                            <tr>
+                                <th>{$categoria['id']}</th>
+                                <th>{$categoria['nome']}</th>
+                                <td style='font-weight: bolder;'>{$categoria['cor']}</td>
+                                <td><a class='btn btn-danger' href='excluir/{$categoria['id']}'>x</a></td>
+                                <td><a class='btn btn-primary' href='editar/{$categoria['id']}'>+</a></td>
+                                
+                            </tr>
+                        ";
+                    }                      
+                ?>
+            </tbody>            
+        </table>
+    </div>
+</div>
