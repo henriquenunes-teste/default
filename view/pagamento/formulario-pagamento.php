d4bfc6ac7ecbd2a7e8e892954da941320b58393b
daniloalbernaz-web <danilo.albernaz@estudante.ifms.edu.br>
2026-05-14 07:25:29 -0400

    Update formulario-pagamento.php

--- view/pagamento/formulario-pagamento.php
+++ view/pagamento/formulario-pagamento.php
@@ -1,25 +1,24 @@
+<div class="container-fluid">
     <h1>Pagamentos</h1>
     <form action="<?php echo APP.'/pagamento/gravar'?>" method="post">
-        <div>
+        <div class="mb-3">
             <label for="id" class="form-label">ID</label>
             <input readonly type="number" class="form-control" name="id" value="<?php echo $dados["id"] ?>">
         </div>
-
-        <div>
+        <div class="mb-3">
             <label for="valor" class="form-label">Valor:</label>
             <span>R$</span>
             <input type="number" step="0.01" class="form-control" name="valor" value="<?php echo $dados["valor"] ?>">
         </div>
-
-        <div>
+        <div class="mb-3">
             <label for="taxa" class="form-label">Taxa:</label>
             <input type="number" step="0.01"  class="form-control" name="taxa" value="<?php echo $dados["valor"] ?>">
             <span>%</span>
         </div>
-        <div>
+        <div class="mb-3">
             <label for="data" class="form-label">Data:</label>
             <input type="date" value="<?php echo $dados["valor"]; ?>" name="data" class="form-control">
         </div>
-
-        <button type="submit" class="btn btn-success">Salvar</button>
+        <button type="submit" class="btn btn-primary">Salvar</button>
     </form>
+</div>
