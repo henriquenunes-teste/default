9598d039d9f2e0a55985f78b294b062eb55f1ed1
daniloalbernaz-web <danilo.albernaz@estudante.ifms.edu.br>
2026-05-14 07:21:15 -0400

    Update listagem-usuarios.php

--- view/usuario/listagem-usuarios.php
+++ view/usuario/listagem-usuarios.php
@@ -1,31 +1,33 @@
-
-    <h1>Usuários</h1>
+<div class="container-fluid">
+    <h1 class="pb-3">Usuários</h1>
     <a href="<?php echo APP."/usuario/novo"; ?>" class="btn btn-success">Novo</a>
     <a href="<?php echo APP."/home"; ?>" class="btn btn-success">Voltar</a>
-    <table class="table">
-        <thead>
-            <tr>
-                <td scope="col">ID</td>
-                <td scope="col">Nome</td>
-                <td scope="col">Senha</td>
-                <td scope="col">Editar</td>
-                <td scope="col">Deletar</td>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-                foreach($usuarios as $usuario){
-                    echo "
-                        <tr>
-                            <td>{$usuario["id"]}</td>
-                            <td>{$usuario["nome"]}</td>
-                            <td>{$usuario["senha"]}</td>
-                            <td><a class='btn btn-primary' href=".APP.'/usuario/editar/'.$usuario["id"].">Editar</a></td>
-                            <td><a class='btn btn-danger' href=".APP.'/usuario/deletar/'.$usuario["id"].">Excluir</a></td>
-                        <tr>
-                    ";
-    
-                }
-            ?>
-        </tbody>
-    </table>
+    <div class="pt-3">
+        <table class="table table-bordered table table-sm table align-middle">
+            <thead>
+                <tr>
+                    <td scope="col">ID</td>
+                    <td scope="col">Nome</td>
+                    <td scope="col">Senha</td>
+                    <td scope="col">Editar</td>
+                    <td scope="col">Deletar</td>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                    foreach($usuarios as $usuario){
+                        echo "
+                            <tr>
+                                <td>{$usuario["id"]}</td>
+                                <td>{$usuario["nome"]}</td>
+                                <td>{$usuario["senha"]}</td>
+                                <td><a class='btn btn-primary' href=".APP.'/usuario/editar/'.$usuario["id"].">Editar</a></td>
+                                <td><a class='btn btn-danger' href=".APP.'/usuario/deletar/'.$usuario["id"].">Excluir</a></td>
+                            <tr>
+                        ";       
+                    }
+                ?>
+            </tbody>
+        </table>
+    </div>
+</div>
