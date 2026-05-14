13d66bf691d4b643e2aea0d3c3b5c9a78b769a6e
daniloalbernaz-web <danilo.albernaz@estudante.ifms.edu.br>
2026-05-14 07:23:48 -0400

    Update formulario-produto.php

--- view/produto/formulario-produto.php
+++ view/produto/formulario-produto.php
@@ -1,23 +1,22 @@
-    <h1>Produtos</h1>
+<div class="container-fluid">
+    <h1>Listagem Produtos</h1>
     <form action="<?php echo APP.'/produto/gravar'?>" method="post">
-        <div>
+        <div class="mb-3">
             <label for="id" class="form-label">ID</label>
             <input readonly type="number" class="form-control" name="id" value="<?php echo $dados["id"] ?>">
         </div>
-
-        <div>
+        <div class="mb-3">
             <label for="nome" class="form-label">Nome:</label>
             <input type="text" class="form-control" name="nome" value="<?php echo $dados["nome"] ?>">
         </div>
-
-        <div>
+        <div class="mb-3">
             <label for="quantidade" class="form-label">Quantidade:</label>
             <input type="number" class="form-control" name="quantidade" value="<?php echo $dados["quantidade"] ?>">
         </div>
-        <div>
+        <div class="mb-3">
             <label for="preco" class="form-label">Preco:</label>
             <input type="number" step="0.01" value="<?php echo $dados["preco"]; ?>" name="preco" class="form-control">
         </div>
-
-        <button type="submit" class="btn btn-success">Salvar</button>
+        <button type="submit" class="btn btn-primary">Salvar</button>
     </form>
+</div>
